e4901c4b6bfa7576cf430b045067606bd04c277a
ttk537 <ttk537@AADW12123>
2024-10-09 11:44:46 -0500

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+    
+
+
+    cout << "Welcome to CSCI 1101. " << endl;
+
+    cout << "Enter your age: ";
+
+    cin >> age;
+
+    cout << "You are " << age << " years old. " << endl;
+
+
+
+
+
+    return 0;
+}
